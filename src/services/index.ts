8be84efdaf3c36c11fbdb46913a1f4a07399b69b
Le Chi Duc <primary.lechiduc@gmail.com>
2022-08-15 08:59:05 +0700

    Update index.ts

--- src/services/index.ts
+++ src/services/index.ts
@@ -3,7 +3,7 @@ import HttpClient from 'utils/HttpClient';
 interface Response {
   result: 'success' | 'error';
 }
-const submitFormToGoogleSheet = async (params: any) => {
+const submitFormToGoogleSheet = async (params: FormData) => {
   return HttpClient.post<FormData, Response>(
     '/macros/s/AKfycbzg0LQCEM5IfC6knVhHDha3uztsVWFDZZ4TlJ2rBIBKVdUQwSVNkGSSfDn6_lDZ2GV0Gg/exec',
     params,
